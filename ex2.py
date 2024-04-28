802ada7ac829d17ea67faf05e980a353707574d7
Sanjay R <122106271+SanjayR14@users.noreply.github.com>
2024-04-28 22:47:39 +0530

    Update ex2.py

--- ex2.py
+++ ex2.py
@@ -1,4 +1,3 @@
-# Definition for singly-linked list.
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
@@ -20,8 +19,6 @@ class Solution:
             n.next = ListNode(val)
             n = n.next
         return root.next
-
-# Function to create a linked list from a list of values
 def create_linked_list(values):
     if not values:
         return None
@@ -31,29 +28,17 @@ def create_linked_list(values):
         current.next = ListNode(val)
         current = current.next
     return head
-
-# Function to print a linked list
 def print_linked_list(head):
     current = head
     while current:
         print(current.val, end=" -> ")
         current = current.next
     print("None")
-
-# Getting input from the user
 input1 = list(map(int, input("Enter values for the first linked list separated by space: ").split()))
 input2 = list(map(int, input("Enter values for the second linked list separated by space: ").split()))
-
-# Creating linked lists
 l1 = create_linked_list(input1)
 l2 = create_linked_list(input2)
-
-# Creating solution object
 sol = Solution()
-
-# Adding two numbers
 result = sol.addTwoNumbers(l1, l2)
-
-# Printing the result
 print("Result:")
 print_linked_list(result)
